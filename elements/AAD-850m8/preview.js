c93b5d7169a9341bdc12115a2802604f2ee448e1
Yuki Mochizuki <yuki.mochizuki.0620@gmail.com>
2023-12-01 10:26:14 +0900

    Update preview.js
    transprantation

--- elements/AAD-850m8/preview.js
+++ elements/AAD-850m8/preview.js
@@ -1,7 +1,7 @@
 function(instance, properties) {
     let preview = '<div id="preview"></div>';
     instance.canvas.append(preview);
-    $("#iframe_container").css({"width":"100vw","height":"100vh","background":"#FFFFFF"});
+    $("#iframe_container").css({"width":"100vw","height":"100vh","background":"#FFFFFF00"});
     $("#preview").css({"background":"url('//dd7tel2830j4w.cloudfront.net/f1647458375771x672274875236648600/wlogo.png') center no-repeat", "background-size":"auto", "width":"100%", "height":"100%"});
 
 }
